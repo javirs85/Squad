052f8e806b4636668fef9e56afd176313945ae05
lordmizel <lordmizell@gmail.com>
2025-03-10 09:50:13 +0100

    Cockpit and missiles
    Added 3D cockpit and missile function.

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float speed;
+    [SerializeField] GameObject explosionPrefab;
+    Vector3 defaultPosition;
+    bool launched = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        defaultPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (launched)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        
+            if(Vector3.Distance(transform.position, target.position) < 1)
+            {
+                Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+                ResetMissile();
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            if(!launched)
+                LaunchMissile();
+        }
+    }
+
+    public void LaunchMissile()
+    {
+        launched = true;
+    }
+
+    void ResetMissile()
+    {
+        transform.position = defaultPosition;
+        launched = false;
+    }
+}

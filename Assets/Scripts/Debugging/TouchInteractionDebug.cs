92b50b49366886925829182e649924c01498d2bc
lordmizel <lordmizell@gmail.com>
2025-03-27 16:12:08 +0100

    Fixed Android interaction.
    Items are now interactive again in the Android build.

--- Assets/Scripts/Debugging/TouchInteractionDebug.cs
+++ Assets/Scripts/Debugging/TouchInteractionDebug.cs
@@ -4,6 +4,7 @@ public class TouchInteractionDebug : MonoBehaviour
 {
     public void ChangeMaterialColor()
     {
+        Debug.Log("I am " + gameObject.name + "! Hear me roar!");
         gameObject.GetComponent<Renderer>().material.color = Color.red;
     }
 }
